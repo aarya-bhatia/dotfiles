d1c57e174bba46f094524c648b6a8a3ed0ec9806
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2023-07-30 21:09:51 -0700

    updates pacman packages when syncing

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -26,3 +26,5 @@ rsync $opts $HOME/.agignore $HOME/dotfiles/agignore
 rsync $opts $HOME/.clang-format $HOME/dotfiles/clang-format
 rsync $opts $HOME/.ssh/config $HOME/.ssh/*.pub $HOME/dotfiles/config/ssh
 rsync $opts $HOME/.config/picom.conf $HOME/dotfiles/config/picom.conf
+
+which pacman >&/dev/null && pacman -Q > $HOME/dotfiles/pacman.txt
