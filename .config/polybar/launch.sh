93f5880fa6541bef947b87c6adf0028389cea349
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2024-03-13 09:29:00 -0500

    Synced on 03/13/2024 09:29:00 AM

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 pgrep -f polybar && polybar-msg cmd quit
 polybar example 2>&1 | tee -a /tmp/polybar.log & disown
+polybar external 2>&1 | tee -a /tmp/polybar1.log & disown
 echo "Polybar launched..."
