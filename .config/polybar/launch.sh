f0701a0ecd87d2e86e85509e614084f9f4e8e0e4
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2024-03-15 11:14:25 -0500

    Synced on 03/15/2024 11:14:25 AM

--- .config/polybar/launch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-pgrep -f polybar && polybar-msg cmd quit
-polybar bspwm 2>&1 | tee -a /tmp/polybar.log & disown
-polybar bspwm1 2>&1 | tee -a /tmp/polybar1.log & disown
-echo "Polybar launched: bspwm bspwm1"
