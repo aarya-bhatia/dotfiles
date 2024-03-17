22d495bcb9c5e11c37af9ddaac5f2c1eb3991722
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2024-03-17 11:55:38 -0500

    Synced on 03/17/2024 11:55:38 AM

--- .config/lemonbar/launchlemon.sh
+++ .config/lemonbar/launchlemon.sh
@@ -5,7 +5,7 @@ start() {
 	cd /home/aarya/.config/lemonbar && make
 	/home/aarya/.config/lemonbar/bin/main | lemonbar -b -p -g x24++ \
 		-F#ffffff -B#222222 -U#268BD2 -u 2 \
-		-f "Hack Nerd Font:size=10" \
+		-f "FreeMono:size=10" \
 		-f "Font Awesome 6 Free"  \
 		-f "Font Awesome 6 Brands" \
 		-f "Font Awesome 6 Free Solid" |
