90bf8fb110f8133381957c448d93f9a7ab51e0da
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2024-03-16 21:49:58 -0500

    Synced on 03/16/2024 09:49:58 PM

--- .config/lemonbar/launchlemon.sh
+++ .config/lemonbar/launchlemon.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-set -e
 source /home/aarya/pyvenv/bin/activate
-pkill -f launchlemon
-python3 /home/aarya/.config/lemonbar/lemonconfig.py | \
-    lemonbar -p -g x24++ \
+make
+pkill -2 -x launchlemon.sh
+bin/main | lemonbar -b -p -g x24++ \
     -F#ffffff -B#222222 -U#268BD2 -u 2 \
     -f "Hack Nerd Font:size=9.5" \
     -f "Font Awesome 6 Free"  \
     -f "Font Awesome 6 Brands" \
-    -f "Font Awesome 6 Free Solid" \
-    | python3 /home/aarya/.config/lemonbar/lemonhandler.py
+    -f "Font Awesome 6 Free Solid"
