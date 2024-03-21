e14df4c5754ff22e8a33d6732a4e704134a69ecb
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2024-03-21 15:49:21 -0500

    Synced on 03/21/2024 03:49:21 PM

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -2,7 +2,8 @@
 
 xsetroot -cursor_name left_ptr & # set cursor style
 numlockx on # turn on numlock
-~/repos/aarya-bhatia/lemonbar/launchlemon.sh & # launch bottom lemonbar
+~/.config/polybar/bspwm_launch.sh & # launch polybar
+# ~/repos/aarya-bhatia/lemonbar/launchlemon.sh & # launch bottom lemonbar
 ~/scripts/keymaps.sh & # setup keyboard mappings
 ~/scripts/wallpaper.py & # set wallpaper to last used
 
@@ -25,7 +26,6 @@ pgrep -f redshift || ~/scripts/nightlight.sh auto &
 
 # start screen locker
 pgrep -f xss-lock || xss-lock --transfer-sleep-lock -- i3lock --nofork --ignore-empty-password --no-unlock-indicator --color=505050 &
-# pgrep -f xss-lock || xss-lock --transfer-sleep-lock -- slock &
 
 # power management
 pgrep -f xfce4-power-manager || xfce4-power-manager --daemon &
--- .config/polybar/bspwm_launch.sh
+++ .config/polybar/bspwm_launch.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
+source /home/aarya/pyvenv/bin/activate
 pgrep -f polybar && polybar-msg cmd quit
-polybar bspwm 2>&1 >/tmp/polybar.log & disown
-MONITOR=HDMI-1 polybar bspwm 2>&1 >/tmp/polybar1.log &
+
+polybar bspwm 2>&1 >> /tmp/polybar.log & disown
+polybar bottom 2>&1 >> /tmp/polybar.log & disown
+
+if xrandr | grep "HDMI-1 connected"; then
+	MONITOR=HDMI-1 polybar bspwm 2>&1 >> /tmp/polybar.log & disown
+	MONITOR=HDMI-1 polybar bottom 2>&1 >> /tmp/polybar.log & disown
+fi
+
 echo "Polybar launched..."
