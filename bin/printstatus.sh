c7e6d8ea93f510dac34e09e908ef6757590d6476
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2024-11-12 21:26:33 -0800

    update status line and add icons

---
+++ bin/printstatus.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# echo $(uname -n -r) Vol:$(sb-volume) Mem:$(sb-memory) Bat:$(sb-battery) $(sb-disk) Temp:$(sb-weather) $(sb-mpd) $(sb-date)
+echo -e "$(sb-mpd) |  $(sb-volume) | 󰍛 $(sb-memory) | $(sb-battery) |  $(sb-weather 2>/dev/null) | $(sb-date)"
--- bin/statusinfo.sh
+++ bin/statusinfo.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-
-Status() {
-    echo Vol:$(sb-volume) Mem:$(sb-memory) Bat:$(sb-battery) $(sb-disk) Temp:$(sb-weather) $(sb-date)
-}
-
 while :; do
-    xsetroot -name "$(Status)"
+    xsetroot -name "$(printstatus.sh)"
     sleep 1
 done
