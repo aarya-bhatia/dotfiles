7b111cb057036398c81ccf123f2d1dece4808e07
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2024-11-28 13:02:19 -0800

    update script

--- bin/wallpaper.sh
+++ bin/wallpaper.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-# file=~/Wallpapers/0170.jpg
-# file=~/Wallpapers/0359.jpg
-# file=~/Wallpapers/0318.jpg
-# file=~/Wallpapers/0211.jpg
-file=~/Wallpapers/0064.jpg
+wpfile=/home/aarya/.wallpaper
+file=/home/aarya/Wallpapers/0328.jpg
+
+if [ -f $wpfile ]; then
+    echo found wallpaper file: $wpfile
+    file=$(tail -1 $wpfile)
+fi
 
 if [ ! -f "$file" ]; then
     file="$(find ~/Wallpapers/ -type f | sort -R | head -1)"
 fi
 
+echo setting wallpaper to: $file
 xwallpaper --stretch "$file"
