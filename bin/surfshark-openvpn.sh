174ff2897e84187e0cb8fe97d9bff74f0efc068e
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2025-01-07 21:38:32 -0800

    update vpn script with 2 locations

--- bin/surfshark-openvpn.sh
+++ bin/surfshark-openvpn.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-gpg -d ~/credentials.openvpn.gpg | sudo openvpn --config ~/Downloads/us-chi.prod.surfshark.comsurfshark_openvpn_udp.ovpn --auth-user-pass /dev/stdin
+set -e
+cd ~/openvpn
+
+# Fastest
+gpg -d credentials.openvpn.gpg | sudo openvpn --config us-chi.prod.surfshark.comsurfshark_openvpn_udp.ovpn --auth-user-pass /dev/stdin
+
+# India
+# gpg -d credentials.openvpn.gpg | sudo openvpn --config in-del.prod.surfshark.comsurfshark_openvpn_udp.ovpn --auth-user-pass /dev/stdin
