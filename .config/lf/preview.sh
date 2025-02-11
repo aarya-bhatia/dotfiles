b30c533d6cf0f6a0280b51cd4faff3d6817b975c
Aarya Bhatia <aarya.bhatia1678@gmail.com>
2025-02-10 22:01:30 -0800

    lf: add linkfrom and linkto command to make symlinks

--- .config/lf/preview.sh
+++ .config/lf/preview.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 case "$1" in
-	text/* | */xml) bat -p --theme=ansi "$1" ;;
-    *.tar*) tar tf "$1";;
-    *.zip) unzip -l "$1";;
-    *.rar) unrar l "$1";;
-    *.8z) 7z l "$1";;
-    *.pdf) pdftotext "$1" -;;
-    *.json) jq < "$1" ;;
-    *.jpg|*.png|*.jpeg) viu $1 ;;
-    *) cat "$1";;
+  text/* | */xml) cat "$1" ;;
+  *.tar*) tar tf "$1";;
+  *.zip) unzip -l "$1";;
+  *.rar) unrar l "$1";;
+  *.8z) 7z l "$1";;
+  *.pdf) pdftotext "$1" -;;
+  *.json) jq < "$1" ;;
+  *.jpg|*.png|*.jpeg) file $1 ;;
+  *) cat "$1";;
 esac
